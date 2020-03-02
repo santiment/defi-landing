5639a1f525852c9e83815f379d8131b0f4ea1c03
haritonasty <a.kharitonovaspb@gmail.com>
2020-03-02 13:38:18 +0300

    products tooltip

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,6 +3,7 @@ import { injectIntl, Link } from 'gatsby-plugin-intl'
 import { tr } from '../../utils/translate'
 import styles from './Header.module.scss'
 import AccountBtn from '../AccountBtn/AccountBtn'
+import SantimentProductsTooltip from '../SantimentProductsTooltip/SantimentProductsTooltip'
 import santimentSvg from '../../images/santiment.svg'
 import logo from './EthCC_Logo.svg'
 
@@ -17,9 +18,11 @@ const Header = ({ isAccountPage, intl }) => {
           <img src={logo} alt='EthCC' />
         </div>
         <div className={styles.right}>
-          <Link className={styles.link} to='/#products'>
-            {tr('header.products', "Products")}
-          </Link>
+          <div className={styles.link}>
+          <SantimentProductsTooltip intl={intl}>
+            <div>Products</div>
+          </SantimentProductsTooltip>
+          </div>
           <a className={styles.link} href='mailto:support@santiment.net'>
             {tr('header.support')}
           </a>
---
+++ src/components/SantimentProductsTooltip/SantimentProductsTooltip.js
@@ -0,0 +1,107 @@
+import React, { useState } from 'react'
+import cx from 'classnames'
+import Tooltip from '@santiment-network/ui/Tooltip'
+import Icon from '@santiment-network/ui/Icon'
+import sanbaseLogoImg from './../../images/logos/logo-sanbase.svg'
+import sheetsLogoImg from './../../images/logos/logo-sheets.svg'
+import neuroLogoImg from './../../images/logos/logo-neuro.svg'
+import styles from './SantimentProductsTooltip.module.scss'
+
+const PRODUCTS = [
+  {
+    img: sanbaseLogoImg,
+    title: 'Sanbase',
+    description: 'header.product.sanbase',
+    to: 'https://app.santiment.net',
+  },
+  {
+    img: sheetsLogoImg,
+    title: 'Sansheets',
+    description: 'header.product.sheets',
+    to: 'https://sheets.santiment.net',
+
+  },
+  {
+    img: neuroLogoImg,
+    title: 'SanAPI',
+    description: 'header.product.neuro',
+    to: 'https://neuro.santiment.net',
+  }
+]
+
+const ProductItem = ({
+                       intl,
+                       product: { to, img, title, description },
+                       className
+                     }) => {
+  return (
+    <a className={cx(styles.wrapper, className)} href={to}>
+      <div className={cx(styles.product, styles.wrapper__product)}>
+        {img && <img className={styles.product__img} src={img} alt={title} />}
+        <div className={styles.product__info}>
+          <div className={styles.product__title}>{title}</div>
+          <div className={styles.product__description}>{intl.formatMessage({ id: description })}</div>
+            <MakeLink
+              className={cx(styles.wrapper__link)}
+              to={to}
+              as={'div'}
+              title={'Go to ' + title}
+            />
+        </div>
+      </div>
+    </a>
+  )
+}
+
+
+const MakeLink = ({ to, title, className, as: El = 'a' }) => (
+  <El href={to} className={cx(styles.link, className)}>
+    {title} <Icon className={styles.linkArrow} type='pointer-right' />
+  </El>
+)
+
+const OpenTrigger = () => <Icon type='arrow-down' className={styles.arrowIcon} />
+const CloseTrigger = () => <Icon type='arrow-up' className={styles.arrowIcon}/>
+
+const SantimentProductsTooltip = ({ className, intl, children }) => {
+  const [isOpen, setOpen] = useState(false)
+
+  return (
+    <Tooltip
+      passOpenStateAs='isActive'
+      closeTimeout={150}
+      position='bottom'
+      align='start'
+      offsetY={7}
+      className={styles.tooltip}
+      trigger={
+        <div className={cx(className, styles.trigger)}>
+          {children}
+          <div className={cx(styles.arrow, isOpen && styles.opened)}>
+            {isOpen ? <CloseTrigger /> : <OpenTrigger />}
+          </div>
+        </div>
+      }
+      onOpen={() => {
+        setOpen(true)
+      }}
+      onClose={() => {
+        setOpen(false)
+      }}
+    >
+      <div className={styles.container}>
+        <div className={styles.header}>
+          <div className={styles.title}>{intl.formatMessage({ id: 'header.products' })}</div>
+          <MakeLink to='https://santiment.net' title={intl.formatMessage({ id: 'header.santiment.goto' })}/>
+        </div>
+        <div className={styles.products}>
+          {PRODUCTS.map((item, index) => (
+            <ProductItem key={index} product={item} intl={intl} />
+          ))}
+        </div>
+      </div>
+    </Tooltip>
+  )
+}
+
+export default SantimentProductsTooltip
